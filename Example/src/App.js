3f5c2fcb5c5728faee8b7f1a8a89c79c9f1f5106
karol-bisztyga <karol.bisztyga@swmansion.com>
2020-07-08 09:47:50 +0200

    tests main screen + simple test

--- Example/src/App.js
+++ Example/src/App.js
@@ -75,6 +75,7 @@ function MainScreen({ navigation }) {
       renderItem={(props) => (
         <MainScreenItem
           {...props}
+          screens={ SCREENS }
           onPressItem={({ key }) => navigation.navigate(key)}
         />
       )}
@@ -88,15 +89,15 @@ MainScreen.navigationOptions = {
   title: '🎬 Reanimated 2.x Examples',
 };
 
-function ItemSeparator() {
+export function ItemSeparator() {
   return <View style={styles.separator} />;
 }
 
-function MainScreenItem({ item, onPressItem }) {
+export function MainScreenItem({ item, onPressItem, screens }) {
   const { key } = item;
   return (
     <RectButton style={styles.button} onPress={() => onPressItem(item)}>
-      <Text style={styles.buttonText}>{SCREENS[key].title || key}</Text>
+      <Text style={styles.buttonText}>{screens[key].title || key}</Text>
     </RectButton>
   );
 }
@@ -130,7 +131,7 @@ const ExampleApp = createSwitchNavigator({
   Reanimated1,
 });
 
-const styles = StyleSheet.create({
+export const styles = StyleSheet.create({
   list: {
     backgroundColor: '#EFEFF4',
   },
